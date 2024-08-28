326907277cb812b3d4d3791fbc2fabd6920ab3a6
Abdulalim Mohamed <abdoleemos200@gmail.com>
2024-08-28 14:45:32 +0300

    :zap:

--- js/script.js
+++ js/script.js
@@ -51,7 +51,20 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = `<div class='container d-flex row'>
+                                                    <div class='days block col-md-6'>
+                                                        ${days}<br>Days
+                                                    </div>
+                                                    <div class='hours block col-md-6'>
+                                                        ${hours}<br>Hours
+                                                    </div>
+                                                    <div class='minutes block col-md-6'>
+                                                    ${minutes}<br>Minutes
+                                                    </div>
+                                                    <div class='seconds block col-md-6'>
+                                                    ${seconds }<br>Seconds
+                                                    </div>
+                                                </div>`;
 
     // If the count down is over, write some text
     if (distance < 0) {
